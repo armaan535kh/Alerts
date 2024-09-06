2a3083afbbb806a0f22549b7eb1627bdaa463a78
armaan535kh <kh.armaan535@gmail.com>
2024-09-05 21:10:32 -0700

    Initial Commit

---
+++ Alerts/AlertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertsApp.swift
+//  Alerts
+//
+//  Created by Armaan Khan  on 05/09/24.
+//
+
+import SwiftUI
+
+@main
+struct AlertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
